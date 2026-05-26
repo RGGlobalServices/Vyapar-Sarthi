5fc393cf011808ead8de026a2da38b7d53acb998
CodeWeft-Technologies <codeweft.ai@gmail.com>
2026-05-26 15:50:47 +0530

    Fix create_engine typo and pin Python 3.12 for Render

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,11 +1,10 @@
-from sqlalchemy import create_env
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./kirana.db")
 
-from sqlalchemy import create_engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
